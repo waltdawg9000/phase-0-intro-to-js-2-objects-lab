04f2bf79ef229fc492bf15593a0e57f179a1d5a8
Walter Morris <waltercmorris18@gmail.com>
2021-09-09 18:12:00 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+const employee = {
+    name: "Walter",
+    streetAddress: "110 West"
+};
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    return Object.assign({},obj,{[key]:value});
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj,key,value){
+    obj[key]=value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(obj, key){
+    const newEmployee = {...obj, ...key};
+    delete newEmployee[key]
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj,key){
+    delete obj[key]
+    return obj;
+}
